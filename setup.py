a038bee96e3b55a91240f04361da50eacb1f13f1
nir0s <nir36g@gmail.com>
2014-06-17 09:45:41 +0300

    initial fabric plugin commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+#########
+# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  * See the License for the specific language governing permissions and
+#  * limitations under the License.
+
+__author__ = 'nir0s'
+
+from setuptools import setup
+
+setup(
+    name='cloudify-fabric-plugin',
+    version='1.0',
+    author='nir0s',
+    author_email='nirc@gigaspaces.com',
+    packages=['fabric_plugin'],
+    license='LICENSE',
+    description='Plugin for reomtely running fabric tasks and command',
+    install_requires=[
+        'cloudify-plugins-common==3.0',
+    ]
+)
