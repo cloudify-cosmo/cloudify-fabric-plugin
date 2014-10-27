d3bd9ada81a85a4d61cae3c8df11ecfb2545e3a2
opencm <limor@gigaspaces.com>
2014-10-27 17:37:07 +0200

    Bump version to 1.1rc1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-fabric-plugin',
-    version='3.1rc1',
+    version='1.1rc1',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['fabric_plugin'],
