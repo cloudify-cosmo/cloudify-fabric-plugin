624aef9d217bcfcbbb9b1c8362b2c47b5bf7d7a3
Noa Kuperberg <noakup@gmail.com>
2016-05-19 18:52:46 +0300

    changes per review

--- fabric_plugin/tests/blueprint/fabric_tasks.py
+++ fabric_plugin/tests/blueprint/fabric_tasks.py
@@ -10,10 +10,7 @@ def test_task_properties(arg):
 
 
 def test_implicit_host_string():
-    # TODO: use a different value, internal_manager_host is canceled
-    # ctx.instance.runtime_properties['expected_host_string'] = \
-    #     ctx.instance.internal_manager_host
     ctx.instance.runtime_properties['expected_host_string'] = \
-        ctx.instance.private_ip
+        ctx.instance.host_ip
 
 non_callable = 1
