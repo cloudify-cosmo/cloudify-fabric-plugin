1ef3f3aa95f3e3f81ddf5ece4f8de7b064caaa32
Nir Cohen <nir0s@users.noreply.github.com>
2016-06-01 20:24:23 +0300

    Removed dev0 suffix before release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-fabric-plugin',
-    version='1.4.1.dev0',
+    version='1.4.1',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['fabric_plugin'],
