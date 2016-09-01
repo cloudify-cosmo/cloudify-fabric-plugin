a7bbc140818962cd39f1439dcbf3297f4391b264
Dan Kilman <dank@gigaspaces.com>
2016-09-01 14:51:18 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-fabric-plugin',
-    version='1.4.1',
+    version='1.4.2',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['fabric_plugin'],
