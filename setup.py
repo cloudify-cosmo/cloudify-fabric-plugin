317d3532056a416ad21f4df522e804bba80f60d4
opencm <opencm@gigaspaces.com>
2018-05-30 15:41:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-fabric-plugin',
-    version='1.5.2',
+    version='9.9.9',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
     packages=['fabric_plugin'],
