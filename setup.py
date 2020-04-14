e01bf1909707cb0c3f7be38aac9f6c44e9ab6d35
Mohammed AbuAisha <mohammeda@cloudify.co>
2020-04-14 18:07:15 +0300

    Bump 2.0.1 version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup
 
 setup(
     name='cloudify-fabric-plugin',
-    version='2.0.0',
+    version='2.0.1',
     author='Cloudify',
     author_email='hello@cloudify.co',
     packages=['fabric_plugin'],
