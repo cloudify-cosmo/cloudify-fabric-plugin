534ce654c779af5127b12857f5e37842289e7348
Nelynehemia <nelyne1014@gmail.com>
2023-03-29 11:13:09 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -18,12 +18,6 @@ import pathlib
 from setuptools import setup
 
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
 def get_version():
     current_dir = pathlib.Path(__file__).parent.resolve()
     with open(os.path.join(current_dir, 'fabric_plugin/__version__.py'),
